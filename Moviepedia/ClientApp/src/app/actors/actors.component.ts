492652e3543c0c3bb0c7dd859acbe519fe0bfe67
ASNET\mistrate <madalina.istrate@edenred.com>
2020-12-30 00:38:42 +0200

    Frontend for get and delete actors

--- Moviepedia/ClientApp/src/app/actors/actors.component.ts
+++ Moviepedia/ClientApp/src/app/actors/actors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActorDTO } from '../shared/models/actor-dto';
+import { ActorService } from '../shared/services/actor.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-actors',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actors.component.css']
 })
 export class ActorsComponent implements OnInit {
+  actors: Array<ActorDTO> = new Array<ActorDTO>();
 
-  constructor() { }
+  constructor(private _actorService: ActorService) { }
 
   ngOnInit() {
+    this.getActors();
+  }
+
+  getActors() {
+    this._actorService.getActors().subscribe((data: ActorDTO[]) => {
+      this.actors = data;
+    })
+  }
+
+  deleteActor(actorId: string) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#cc99ff',
+      cancelButtonColor: '#a64dff',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this._actorService.deleteActor(actorId).subscribe(() => {
+          Swal.fire({
+            title: 'Deleted!',
+            text: 'Actor has been deleted.',
+            icon: 'success',
+            confirmButtonColor: '#a64dff'
+          }
+          )
+        });
+        this.getActors();
+      }
+    })
   }
 
 }
